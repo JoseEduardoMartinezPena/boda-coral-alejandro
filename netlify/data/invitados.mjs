bb83c6e8a21ef2a7000533a15bea5351793097f9
Jose Martinez <pepepena@live.com.mx>
2026-09-08 19:24:10 -0600

    act: añadidos enlaces y lista de invitados

--- netlify/data/invitados.mjs
+++ netlify/data/invitados.mjs
@@ -1,19 +1,752 @@
 export const invitaciones = {
-  "demo-a7f82c91e4": {
+  "1-pkLt-GhPRI": {
     id: "INV-001",
-    nombre: "Familia López",
-    pases: 4,
+    nombre: "Abraham Macias",
+    pases: 1,
   },
 
-  "demo-b93d71af20": {
+  "FSH92HVfOB3A": {
     id: "INV-002",
-    nombre: "María González",
+    nombre: "Adolfo Perea",
     pases: 2,
   },
 
-  "demo-c18e52fd77": {
+  "5zNFCLVwoA9d": {
     id: "INV-003",
-    nombre: "Alejandro Pérez",
+    nombre: "Adrian Malpica",
     pases: 1,
   },
+
+  "aCZhI8c80LfJ": {
+    id: "INV-004",
+    nombre: "German Arroyo",
+    pases: 4,
+  },
+
+  "J-UG6gsGY1Jl": {
+    id: "INV-005",
+    nombre: "Alejandra Lopez",
+    pases: 2,
+  },
+
+  "NhNHVEDN2sOu": {
+    id: "INV-006",
+    nombre: "Alejandro Casco Castillo",
+    pases: 2,
+  },
+
+  "EJYu9PA1E3jM": {
+    id: "INV-007",
+    nombre: "Alejandro Casco Gonzalez",
+    pases: 2,
+  },
+
+  "59_YAYvdklPN": {
+    id: "INV-008",
+    nombre: "Alejandro Gutierrez",
+    pases: 2,
+  },
+
+  "EYasSv22dtrt": {
+    id: "INV-009",
+    nombre: "Alejandro Ramirez",
+    pases: 2,
+  },
+
+  "v46hDebNix52": {
+    id: "INV-010",
+    nombre: "Alexis Cervantes",
+    pases: 2,
+  },
+
+  "MfErkosOulVF": {
+    id: "INV-011",
+    nombre: "Alexis Sanchez",
+    pases: 2,
+  },
+
+  "4gE6jOZ_aTPj": {
+    id: "INV-012",
+    nombre: "Alfredo Arenas Prosperi",
+    pases: 3,
+  },
+
+  "9S5LoZthULuZ": {
+    id: "INV-013",
+    nombre: "Anahi Rangel",
+    pases: 3,
+  },
+
+  "p-OIxGe-r4Ol": {
+    id: "INV-014",
+    nombre: "Andrea Trejo",
+    pases: 2,
+  },
+
+  "hXweebSvppvz": {
+    id: "INV-015",
+    nombre: "Angelica Blanco",
+    pases: 1,
+  },
+
+  "nOI6qVBrSo6g": {
+    id: "INV-016",
+    nombre: "Antonio Flores",
+    pases: 1,
+  },
+
+  "6p5yiXjtfvx6": {
+    id: "INV-017",
+    nombre: "Joseph Rigel Calva",
+    pases: 2,
+  },
+
+  "33piz0LPcuNV": {
+    id: "INV-018",
+    nombre: "Ariadna Hernandez",
+    pases: 2,
+  },
+
+  "IbU123m-fYnA": {
+    id: "INV-019",
+    nombre: "Marco Antonio Arruti",
+    pases: 1,
+  },
+
+  "-f0jEo07dT2l": {
+    id: "INV-020",
+    nombre: "Arturo Gonzalez Flores",
+    pases: 4,
+  },
+
+  "CDCBSy40LDKr": {
+    id: "INV-021",
+    nombre: "Arturo Gonzalez Coronado",
+    pases: 2,
+  },
+
+  "pJBUbNnd7H6f": {
+    id: "INV-022",
+    nombre: "Roberto Crespo",
+    pases: 2,
+  },
+
+  "dpTBHEJfn84E": {
+    id: "INV-023",
+    nombre: "Brayan Agudelo",
+    pases: 2,
+  },
+
+  "DHp9nDvxjN6y": {
+    id: "INV-024",
+    nombre: "Brenda Morales",
+    pases: 2,
+  },
+
+  "qRvk-wPtMBpR": {
+    id: "INV-025",
+    nombre: "Catalina blanco",
+    pases: 1,
+  },
+
+  "ZewhnDR81cBf": {
+    id: "INV-026",
+    nombre: "Maria de Jesus Cruz Herrera",
+    pases: 1,
+  },
+
+  "EvdgmpbWO33F": {
+    id: "INV-027",
+    nombre: "Cinthya Baltazar",
+    pases: 2,
+  },
+
+  "8_X0fTmWBxNv": {
+    id: "INV-028",
+    nombre: "Claudia Casco",
+    pases: 1,
+  },
+
+  "cUq88NF34pWt": {
+    id: "INV-029",
+    nombre: "Cuitlahuac Meza",
+    pases: 1,
+  },
+
+  "6eUgyE3G4b1n": {
+    id: "INV-030",
+    nombre: "Daniel Romero Planter",
+    pases: 2,
+  },
+
+  "fxFtisu5-4IS": {
+    id: "INV-031",
+    nombre: "Daniela Gomez",
+    pases: 2,
+  },
+
+  "x0U6-ETneAyc": {
+    id: "INV-032",
+    nombre: "David Lopez",
+    pases: 3,
+  },
+
+  "624zLoyvNvMN": {
+    id: "INV-033",
+    nombre: "David Varela",
+    pases: 2,
+  },
+
+  "JprDcw_66xO7": {
+    id: "INV-034",
+    nombre: "Carlos Corro",
+    pases: 2,
+  },
+
+  "qaH0opjCpFqZ": {
+    id: "INV-035",
+    nombre: "Diego carbonell",
+    pases: 2,
+  },
+
+  "JpMalfF_Bcag": {
+    id: "INV-036",
+    nombre: "Diego Duran",
+    pases: 1,
+  },
+
+  "UMhaAEs4i4Lq": {
+    id: "INV-037",
+    nombre: "Dulce Gonzalez",
+    pases: 1,
+  },
+
+  "MSJIjd_Xvmw-": {
+    id: "INV-038",
+    nombre: "Eduardo Chacon",
+    pases: 1,
+  },
+
+  "f51JF4_4GJC6": {
+    id: "INV-039",
+    nombre: "Eduardo Romero",
+    pases: 2,
+  },
+
+  "kusvfyEfepK9": {
+    id: "INV-040",
+    nombre: "Efren Aguirre",
+    pases: 2,
+  },
+
+  "DT1XGdYFPVqn": {
+    id: "INV-041",
+    nombre: "Emmanuel Alvarez",
+    pases: 1,
+  },
+
+  "8ljiQiYqf9K1": {
+    id: "INV-042",
+    nombre: "Erick Sanchez Felix",
+    pases: 2,
+  },
+
+  "kJznnMp3j8hM": {
+    id: "INV-043",
+    nombre: "Erika Macias",
+    pases: 2,
+  },
+
+  "32sHGbePqL4R": {
+    id: "INV-044",
+    nombre: "Ernestina Gonzalez",
+    pases: 1,
+  },
+
+  "RboAi5e5aii3": {
+    id: "INV-045",
+    nombre: "Estefania Barrera",
+    pases: 3,
+  },
+
+  "Hd5JVU_Squbu": {
+    id: "INV-046",
+    nombre: "Eugenio Casco",
+    pases: 1,
+  },
+
+  "yw7yj3dsj1ID": {
+    id: "INV-047",
+    nombre: "Fabian Hernandez",
+    pases: 2,
+  },
+
+  "0ZvoKJLogHea": {
+    id: "INV-048",
+    nombre: "Fernanda Millan",
+    pases: 2,
+  },
+
+  "D9wA3Bnc3tOO": {
+    id: "INV-049",
+    nombre: "Fernando Fdz",
+    pases: 2,
+  },
+
+  "1udNZamfsaDI": {
+    id: "INV-050",
+    nombre: "Georgina Blanco",
+    pases: 1,
+  },
+
+  "0YvsjtlT83XU": {
+    id: "INV-051",
+    nombre: "Georgina Perez",
+    pases: 2,
+  },
+
+  "CX6_aY30hGQc": {
+    id: "INV-052",
+    nombre: "Gisela Garcia",
+    pases: 2,
+  },
+
+  "uUOlEFgi1IVp": {
+    id: "INV-053",
+    nombre: "Guillermo Casco Castillo",
+    pases: 1,
+  },
+
+  "cCykrCJJYDTa": {
+    id: "INV-054",
+    nombre: "Harumy Lezama",
+    pases: 2,
+  },
+
+  "6WAOaPLIUT5r": {
+    id: "INV-055",
+    nombre: "Hector Garcia",
+    pases: 3,
+  },
+
+  "w8MsFEYQQhRt": {
+    id: "INV-056",
+    nombre: "Humberto Macias",
+    pases: 2,
+  },
+
+  "C--TV1SU1XNj": {
+    id: "INV-057",
+    nombre: "Jose Luis Rangel",
+    pases: 2,
+  },
+
+  "JNbQaxgIv6qR": {
+    id: "INV-058",
+    nombre: "Irwing Cajica",
+    pases: 2,
+  },
+
+  "AYtJ56aZbpln": {
+    id: "INV-059",
+    nombre: "Itzel Aguilar",
+    pases: 2,
+  },
+
+  "iDfZQe01LYIq": {
+    id: "INV-060",
+    nombre: "Ivan Casco",
+    pases: 2,
+  },
+
+  "m0Ved7Byy4tJ": {
+    id: "INV-061",
+    nombre: "Ivanhoe Valdovinos",
+    pases: 1,
+  },
+
+  "ymCh8UU8LH-U": {
+    id: "INV-062",
+    nombre: "Jaqueline Arellano",
+    pases: 2,
+  },
+
+  "Jzkv4BqUmHID": {
+    id: "INV-063",
+    nombre: "Javier Merchan",
+    pases: 2,
+  },
+
+  "-YyIwTpuQCRn": {
+    id: "INV-064",
+    nombre: "Kylee Hayton",
+    pases: 2,
+  },
+
+  "8YAkaGoe_djg": {
+    id: "INV-065",
+    nombre: "Jorge Bala",
+    pases: 1,
+  },
+
+  "Z3bdlOXzQFw3": {
+    id: "INV-066",
+    nombre: "Luciano Stefano Serra",
+    pases: 1,
+  },
+
+  "MuND6P0lLQd9": {
+    id: "INV-067",
+    nombre: "Jose Lopez",
+    pases: 1,
+  },
+
+  "pC6iXPuYxI3l": {
+    id: "INV-068",
+    nombre: "Luis Carpinteyro",
+    pases: 2,
+  },
+
+  "YPoPSJSdgz5o": {
+    id: "INV-069",
+    nombre: "Juan Carlos Garcia",
+    pases: 1,
+  },
+
+  "xfo7-NxNUqlK": {
+    id: "INV-070",
+    nombre: "Julio Flores",
+    pases: 2,
+  },
+
+  "XL2rJsbF79OE": {
+    id: "INV-071",
+    nombre: "Kathya Rangel",
+    pases: 2,
+  },
+
+  "fxFwm1urKLiv": {
+    id: "INV-072",
+    nombre: "Laura Trujeque",
+    pases: 1,
+  },
+
+  "308dM_ZX_U_u": {
+    id: "INV-073",
+    nombre: "Leonardo Ariel Oyola",
+    pases: 1,
+  },
+
+  "HAh6W4ywaWEZ": {
+    id: "INV-074",
+    nombre: "Liliana Garcia",
+    pases: 1,
+  },
+
+  "4wlHbPRw9Z8u": {
+    id: "INV-075",
+    nombre: "Luz Maria Gonzalez",
+    pases: 1,
+  },
+
+  "yosUPq2bzgaj": {
+    id: "INV-076",
+    nombre: "Maria Auxilio Delgado Bada",
+    pases: 1,
+  },
+
+  "6ei_SRCtaiqD": {
+    id: "INV-077",
+    nombre: "Manolo Casco",
+    pases: 1,
+  },
+
+  "epZZHIaBZH1M": {
+    id: "INV-078",
+    nombre: "Roger Caraballo",
+    pases: 2,
+  },
+
+  "0du2DDHe6tGu": {
+    id: "INV-079",
+    nombre: "Jardel Tecanhuey",
+    pases: 2,
+  },
+
+  "0N-S04eMbANa": {
+    id: "INV-080",
+    nombre: "Mariana Gomez",
+    pases: 4,
+  },
+
+  "0Tj9ZATatuO9": {
+    id: "INV-081",
+    nombre: "Mariana Limon",
+    pases: 2,
+  },
+
+  "lE-lByNWLFJQ": {
+    id: "INV-082",
+    nombre: "Marilu Perez",
+    pases: 1,
+  },
+
+  "47pUs7XBELZ9": {
+    id: "INV-083",
+    nombre: "Marlis Hayton",
+    pases: 2,
+  },
+
+  "D6O0gC_UwbPq": {
+    id: "INV-084",
+    nombre: "Maria Eugenia Casco",
+    pases: 4,
+  },
+
+  "mgUQj65-gagW": {
+    id: "INV-085",
+    nombre: "Mauricio Gutierrez",
+    pases: 2,
+  },
+
+  "DD5Cq-nd4isT": {
+    id: "INV-086",
+    nombre: "Mauricio Robles",
+    pases: 2,
+  },
+
+  "0frm4-uSe5OU": {
+    id: "INV-087",
+    nombre: "Maura Casco",
+    pases: 1,
+  },
+
+  "V_Jkxj4egYJX": {
+    id: "INV-088",
+    nombre: "Maximiliano Munguia",
+    pases: 2,
+  },
+
+  "ufUqa7yhAY8W": {
+    id: "INV-089",
+    nombre: "Mazaru Lezama Shimizu",
+    pases: 2,
+  },
+
+  "_0c7aoeHIwSe": {
+    id: "INV-090",
+    nombre: "Melany Aranda",
+    pases: 1,
+  },
+
+  "ap4Cy5HBWOjN": {
+    id: "INV-091",
+    nombre: "Guillermo Casco Rodriguez",
+    pases: 2,
+  },
+
+  "LMFmB2U3sHMc": {
+    id: "INV-092",
+    nombre: "Irma Garcia",
+    pases: 1,
+  },
+
+  "REnrm6fSkkO-": {
+    id: "INV-093",
+    nombre: "Pablo Beutelspacher",
+    pases: 2,
+  },
+
+  "5Cv7p-_IbySR": {
+    id: "INV-094",
+    nombre: "Phol Casco",
+    pases: 1,
+  },
+
+  "PqIGfO-KDdXC": {
+    id: "INV-095",
+    nombre: "Raul Colonnier",
+    pases: 1,
+  },
+
+  "VWK4NbBwVoGW": {
+    id: "INV-096",
+    nombre: "Renato duran",
+    pases: 2,
+  },
+
+  "LcWDPOFSyDFo": {
+    id: "INV-097",
+    nombre: "Rodrigo Vallejo",
+    pases: 2,
+  },
+
+  "tmrEEXwu8O0g": {
+    id: "INV-098",
+    nombre: "Rogelio soler",
+    pases: 2,
+  },
+
+  "Duu1wkn_XZzX": {
+    id: "INV-099",
+    nombre: "Jahir Chazari",
+    pases: 2,
+  },
+
+  "61223sfZgkW8": {
+    id: "INV-100",
+    nombre: "Rosa Ahumada",
+    pases: 2,
+  },
+
+  "FPqGnAfA4H9r": {
+    id: "INV-101",
+    nombre: "Ruben Luna Diaz",
+    pases: 1,
+  },
+
+  "dq1vB0Ogiuzj": {
+    id: "INV-102",
+    nombre: "Eduardo Ramirez Martinez",
+    pases: 2,
+  },
+
+  "FZSn2LpTO5EO": {
+    id: "INV-103",
+    nombre: "Sebastian Lezama",
+    pases: 2,
+  },
+
+  "kCgnLcF9Mby4": {
+    id: "INV-104",
+    nombre: "Irwyn Acevedo Ramirez",
+    pases: 2,
+  },
+
+  "Vm5HpOaHzWRR": {
+    id: "INV-105",
+    nombre: "Sutko Cardenas",
+    pases: 2,
+  },
+
+  "QlFxtMBe-yLI": {
+    id: "INV-106",
+    nombre: "Teresa Garcia",
+    pases: 2,
+  },
+
+  "xqHJj0kg7tKR": {
+    id: "INV-107",
+    nombre: "Teresa Shimizu",
+    pases: 1,
+  },
+
+  "p1v47l2vtD7E": {
+    id: "INV-108",
+    nombre: "Irma Romero Garcia",
+    pases: 2,
+  },
+
+  "QDKFpyMLchmt": {
+    id: "INV-109",
+    nombre: "Victor Lezama",
+    pases: 2,
+  },
+
+  "BKDc9suP3Nkg": {
+    id: "INV-110",
+    nombre: "Victor Lima",
+    pases: 3,
+  },
+
+  "-TfD_g38VVi9": {
+    id: "INV-111",
+    nombre: "Itzel Sanchez",
+    pases: 1,
+  },
+
+  "hF_jFUTk_RCz": {
+    id: "INV-112",
+    nombre: "Yolanda Cordeiro",
+    pases: 2,
+  },
+
+  "k_XBkMTxwWpG": {
+    id: "INV-113",
+    nombre: "Diego Espinosa",
+    pases: 1,
+  },
+
+  "A1p5q7sX4-RP": {
+    id: "INV-114",
+    nombre: "Daniel Zeledon",
+    pases: 2,
+  },
+
+  "hcynGXLc9DKa": {
+    id: "INV-115",
+    nombre: "Gerardo Sanchez Barrios",
+    pases: 3,
+  },
+
+  "ltxMZ0C33cGb": {
+    id: "INV-116",
+    nombre: "Yoshi Aguirre",
+    pases: 2,
+  },
+
+  "qGdbNcDTpSpl": {
+    id: "INV-117",
+    nombre: "Aranzazu Ruíz",
+    pases: 2,
+  },
+
+  "rxcDAc8kB7cp": {
+    id: "INV-118",
+    nombre: "Edgar Daniel Tovar",
+    pases: 2,
+  },
+
+  "zeVhqfgpj5HH": {
+    id: "INV-119",
+    nombre: "Jessica Quiroga",
+    pases: 2,
+  },
+
+  "SicpBZ83HFdp": {
+    id: "INV-120",
+    nombre: "Brenda Villegas",
+    pases: 1,
+  },
+
+  "_1wOVg4dRh2j": {
+    id: "INV-121",
+    nombre: "Ian Valdovinos",
+    pases: 2,
+  },
+
+  "rWkhPRq2HIdU": {
+    id: "INV-122",
+    nombre: "Victor Galicia",
+    pases: 1,
+  },
+
+  "qltSkjgcQnOs": {
+    id: "INV-123",
+    nombre: "Marcos Quiñones",
+    pases: 1,
+  },
+
+  "FhF_UTTdAuek": {
+    id: "INV-124",
+    nombre: "Catalina Delgado",
+    pases: 1,
+  },
+
+  "SbAB8Kq7soqL": {
+    id: "INV-125",
+    nombre: "Diana Damus",
+    pases: 4,
+  },
+
 };
